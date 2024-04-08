88fb8ec4639712fe71341d1efb91668803ade549
Tuan <tuan.kieudangquoc03@hcmut.edu.vn>
2024-04-08 15:24:56 +0700

    add check_col_arr func

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+def check_col_arr(curr:list, constr:list) -> bool:
+    num_of_list = len(constr) - len(curr) + 1
+    for i in range(num_of_list):
+        check = True
+        for j in range(i,len(curr)):
+            if curr[j] > constr[j]: 
+                check = False
+                break
+        curr.insert(0,0)
+        if len(curr) > len(constr): return check
+
+
